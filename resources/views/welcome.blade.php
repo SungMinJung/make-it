30bc0c3ceda23fad1d3524b79b406ffd6e9d9d2d
phkhyun <phkhyun@daum.net>
2019-01-25 14:00:46 +0900

    공통 scss 제작

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,172 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                사진
+            </div>
+            <div class="col-1"></div>
+            <div class="col-4">
+                <div class="row">
+                    사진
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="row">
+                        메이크아이티는 고객의 목적에 맞는 웹과 어플리케이션을<br>
+                        효율적이고 합리적으로 제작하는 전문업체입니다.
                 </div>
             </div>
         </div>
-    </body>
+        <div class="row">
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container">
+            <div class="row">
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+            </div>
+            <div class="row">
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+            </div>
+            <div class="row">
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+                ㅋㅋㅋ<br>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                사진 + 포트폴리오
+            </div>
+        </div>
+        <div class="row">
+            li li li li
+        </div>
+        <div class="row">
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+            <div class="col-2">
+                test<br>test<br>test<br>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                </div>
+                <div class="col-5">
+                    ㅋㅋㅋ<br>
+                    ㅋㅋㅋ<br>
+                    ㅋㅋㅋ<br>
+                    ㅋㅋㅋ<br>
+                    ㅋㅋㅋ<br>
+                </div>
+                                    
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                사진
+            </div>
+            <div class="col-1"></div>
+            <div class="col-4">
+                는 항상 최고의 솔루션을 제공합니다
+            </div>
+        </div>
+    </div>
+</body>
 </html>

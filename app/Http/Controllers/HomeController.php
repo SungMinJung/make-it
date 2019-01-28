9298546b3159819cdf1ba5776f687817186a0646
phkhyun <phkhyun@daum.net>
2019-01-28 14:09:50 +0900

    commom 삭제, 홈컨트롤러 예시 작성

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +23,43 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $portfolio = [
+            [
+                'link'=>'//naver.com',
+                'title'=>'재미니',
+                'main_image'=>'//placehold.it/224x224',
+                'category'=>'app'
+            ],
+            [
+                'link'=>'//naver.com',
+                'title'=>'성미니',
+                'main_image'=>'//placehold.it/224x224',
+                'category'=>'web'
+            ],
+            [
+                'link'=>'//naver.com',
+                'title'=>'현규',
+                'main_image'=>'//placehold.it/224x224',
+                'category'=>'app'
+            ],
+            [
+                'link'=>'//naver.com',
+                'title'=>'동욱이',
+                'main_image'=>'//placehold.it/224x224',
+                'category'=>'web'
+            ],
+            [
+                'link'=>'//naver.com',
+                'title'=>'재미니2',
+                'main_image'=>'//placehold.it/224x224',
+                'category'=>'response'
+            ],
+        ];
+
+        // b:foreach ($portfolio as $item) {
+        //     {{ $item->name }}
+        //     {{ $item['name'] }}
+        // }
+        return view('home',compact('portfolio'));
     }
 }

72a7a40143fdf89475ea4cafcd2741cccf7b5c5a
dongwook <donguk7531@hanmail.net>
2019-01-29 14:12:07 +0900

    3 수정

--- resources/views/main/3.blade.php
+++ resources/views/main/3.blade.php
@@ -33,29 +33,30 @@
     background-color: #3d3d3d;
     color: #ffffff;
 }
+
 </style>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <div class="row" style="background-image:url('/image/mask17.png'); width:1920px; height:330px;">
         <div class="container"> 
-            <h1><p><strong>Mobile Appilcation</p></h1></strong>
-            <h4><p>모바일 어플리케이션</p></h4><br>
-            <button type="submit" class="btn black" style="margin:0px auto;">자세히 알아보기</button>
+            <h1><p><strong>Mobile Appilcation</h1></strong>
+            <h4>모바일 어플리케이션</p></h4><br>
+            <button type="submit" class="btn blue" style="margin:0px auto;">자세히 알아보기</button>
         </div>
     </div>
 
     <div class="row" style="background-image:url('/image/mask18.jpg'); width:1920px; height:330px;">
         <div class="container"> 
-            <h1><p><strong>Mobile Appilcation</p></h1></strong>
-            <h4><p>모바일 어플리케이션</p></h4><br>
-            <button type="submit" class="btn black" style="margin:0px auto;">자세히 알아보기</button>
+            <h1><p><strong>Website</p></h1></strong>
+            <h4><p>웹사이트</p></h4><br>
+            <button type="submit" class="btn blue" style="margin:0px auto;">자세히 알아보기</button>
         </div>
     </div>
     
     <div class="row" style="background-image:url('/image/mask19.jpg'); width:1920px; height:330px;">
         <div class="container"> 
-            <h1><p><strong>Mobile Appilcation</p></h1></strong>
-            <h4><p>모바일 어플리케이션</p></h4><br>
-            <button type="submit" class="btn black" style="margin:0px auto;">자세히 알아보기</button>
+            <h1><p><strong>Responsive Website</p></h1></strong>
+            <h4><p>반응형 웹사이트</p></h4><br>
+            <button type="submit" class="btn blue" style="margin:0px auto;">자세히 알아보기</button>
         </div>
     </div>

4b43bdfb515a33861abf105ab92eae998762fce7
dongwook <donguk7531@hanmail.net>
2019-01-29 12:18:43 +0900

    3,5,6 내용수정

--- resources/views/main/5.blade.php
+++ resources/views/main/5.blade.php
@@ -1,39 +1,47 @@
 <style>
 
-#subject{
-    font-weight: bold;
-    font-size: 19px;
-    padding-top: 20px;
-    padding-bottom: 20px;
+#title{
+    color:blue;
+    font-weight:bold;
+    font-size:20px;
+    text-align:left;
 }
-#command{
-    width: 75px;
-    height: 40px;
-    font-family: NotoSansCJKkr;
-    font-size: 27px;
-    font-weight: 900;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.48;
-    letter-spacing: normal;
-    text-align: left;
-    color: #0066ff;
+#choice{
+    padding-top:120px;
 }
+/* *{
+  outline:1px solid red;
+  padding:0px;
+  margin:0px;
+} */
 </style>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<div class="container" style="background-image:url('/image/acril.jpg');width:1920px;height:480px;padding-top:150px;background-size:contain">
-        <div class="col-5">
+<div class="row" style="background-image:url('/image/acril.jpg'); width:1921px; height:803px;">
+<div class="container">
+        <div class="row">
+            <div class="col-5">
         </div>
-        <div class="col-5">
-            <div class="row">
-            <img src="https://cdn.zeplin.io/5c492537058fa079f196c51a/assets/E788CFC0-2C6B-4413-978F-FD68FF882C5F.png" class="img">
-            <p id="subject"><h3>을 선택해야하는 이유</h3>
-            <p id="command"><h3>다양성</h3><span> variety</span>
+
+            <div class="col-5">
+            <img id="choice" src="https://cdn.zeplin.io/5c492537058fa079f196c51a/assets/E788CFC0-2C6B-4413-978F-FD68FF882C5F.png" style="width:166.5px; height:83px;"><strong>을 선택해야하는 이유</strong><br>
+            <div id="title">다양성 </div>
+            <p ><strong>Variety</strong> </p>
+            <h5>공장에서 찍어내는 듯한 웹/앱 서비스를 제공하지않습니다.</h5>
+            <div id="title"> 신뢰성 </div>
+            <p ><strong>Rebiality</strong> </p>
+            <h5>고객과 끊임없이 커뮤니케이션하며 고객이 가장 원하는 웹/앱 서비스를</h5>
+            <h5>제공합니다</h5>
+            <div id="title"> 효율성 </div>
+            <p ><strong>Efficiency</strong> </p>
+            <h5>가장 합리적인 비용으로 최대의 서비스를 만들어냅니다.</h5>
+            <div id="title">보안성</div>
+            <p ><strong>Secuity</strong> </p>
+            <h5>정보 보호를 위한 엄격한 보안을 준수합니다.</h5>
+            </div>
+
         </div>
-            </div>  
-        </div> 
-    </div>
-</div>
+        
+
 
 
 

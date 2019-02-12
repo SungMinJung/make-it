e1c6f17b27727a817eac81e8a22f8fbe4cb1d643
Yoon Jae Min <woal136@naver.com>
2019-02-12 16:40:03 +0900

    main1 백그라운드 수정 및 간격조절

--- resources/views/main/1.blade.php
+++ resources/views/main/1.blade.php
@@ -5,19 +5,19 @@
 </style>
 
 
-<div class="row" style="margin: 0 auto; background-image:url('https://cdn.zeplin.io/5c492537058fa079f196c51a/assets/8956A192-3EB4-4D06-91EE-C23ABC4F62C9.png');width:1920px;height:480px;padding-top:150px;background-repeat:no-repeat;">
+<div class="row" style="margin: 0 auto; background-image:url('/img/main1bg.jpg');width:1920px;height:480px;padding-top:150px;background-repeat:no-repeat;">
     <div class="container">
         <div class="row" >
             <div class="col-5"></div>
             <div class="row">
                 <div class="col-3">
-                    <img src="img/main1makeit1.svg" alt="" style="width:346px;height:162px;">
+                    <img src="img/main1makeit1.svg" alt="" style="width:346px;height:162px;position:relative;bottom:50px;">
                 </div>
             </div>
             <div class="col-5"></div>
             <div class="row">
                 <div class="col-4">
-                    <p class="title1" style="margin-top:20px;margin-bottom:20px; font-family: 'Noto Sans KR', sans-serif;">
+                    <p class="title1" style="margin-top:20px;margin-bottom:20px; font-family: 'Noto Sans KR', sans-serif;position:relative;bottom:50px;">
                         메이크아이티는 고객의 목적에 맞는 웹과 어플리케이션을
                         효율적이고 합리적으로 제작하는 전문업체입니다.
                     </p>
@@ -26,7 +26,7 @@
             <div class="col-5"></div>
             <div class="row">
                 <div class="col-5">
-                    <p class="content1" style="font-family: 'Noto Sans KR', sans-serif;">
+                    <p class="content1" style="font-family: 'Noto Sans KR', sans-serif;position:relative;bottom:50px;">
                         MAKE IT는<br>
                         고객의 이상적인 웹과 앱을 구현하는 <span class="bluetitle">고객맞춤형(Clinent customized)</span> 제작 서비스입니다. 
                         아울러, <span class="bluetitle">사용자 중심(UX:User experience)</span>의 홈페이지부터 쇼핑몰, 웹 서비스, 어플리케이션 등 사용자가 보다 나은 경험을 할 수 있도록 합니다.
@@ -34,19 +34,16 @@
                 </div>
             </div>
             <div class="col-5"></div>
-            <button type="submit" class="btn blue" style="margin:0px auto;margin-top:20px;" onclick="location.href='{{ url('/Portfolio') }}'">Portfolio</button>
+            <button type="submit" class="btn blue" style="margin:0px auto;margin-top:20px;position:relative;bottom:50px;" onclick="location.href='{{ url('/Portfolio') }}'">Portfolio</button>
         </div>
     </div>
 </div>
-
-<div class="row"><br><br></div>
 
 <div class="container">
     <div class="row">
         <img src="img/MAKEIT1.svg" alt="" style="width:167px;height:83px;"><span class="needbot">를 필요로 하는 고객</span>
     </div>
-    <div class="row"><br></div>
-    <div class="row">
+    <div class="row" style="margin:30px 0px 60px 0px;">
         <div class="col-2 bluebox">
             <img src="/img/num1.svg" alt="" style="" class="topnumber">
             <img src="/img/blue1.svg" alt="" class="topimage">
@@ -74,5 +71,3 @@
         </div>
     </div>
 </div>
-
-<div class="row"><br><br><br></div>

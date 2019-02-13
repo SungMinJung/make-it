ccc9c5a73fdce7530a2c727e20188324ee597783
Yoon Jae Min <woal136@naver.com>
2019-02-13 11:36:40 +0900

    main5 폰트 적용

--- resources/views/main/5.blade.php
+++ resources/views/main/5.blade.php
@@ -4,6 +4,14 @@
 #choice{
     padding-top:120px;
 }
+.main5content{
+    font-size: 20px;
+    font-weight: 900;
+    color: #3d3d3d;
+    font-family: 'Noto Sans KR', sans-serif;
+    position: relative;
+    top: 10px;
+}
 
 </style>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -14,20 +22,20 @@
                 </div>
 
             <div class="col-5">
-                <img id="choice" src="https://cdn.zeplin.io/5c492537058fa079f196c51a/assets/E788CFC0-2C6B-4413-978F-FD68FF882C5F.png" style="width:166.5px; height:83px;"><strong>을 선택해야하는 이유</strong><br>
-                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px;">
-                다양성<span style="font-size:20px; font-family: NotoSansCJKkr; color:#3d3d3d">Variety</span></p>
-                <h4>공장에서 찍어내는 듯한 웹/앱 서비스를 제공하지않습니다.</h4>
-                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px;">
-                신뢰성<span style="font-size:20px; font-family: NotoSansCJKkr; color:#3d3d3d">Reliability</span></p>
-                <h4>고객과 끊임없이 커뮤니케이션하며 고객이 가장 원하는 웹/앱서비스를</h4>
-                <h4>제공합니다</h4>
-                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px;">
-                효율성<span style="font-size:20px; font-family: NotoSansCJKkr; color:#3d3d3d">Efficiency</span></p>
-                <h4>가장 합리적인 비용으로 최대의 서비스를 만들어냅니다.</h4>
-                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px;">
-                보안성<span style="font-size:20px; font-family: NotoSansCJKkr; color:#3d3d3d">Security</span> </p>
-                <h4>정보 보호를 위한 엄격한 보안을 준수합니다.</h4>
+                <img id="choice" src="https://cdn.zeplin.io/5c492537058fa079f196c51a/assets/E788CFC0-2C6B-4413-978F-FD68FF882C5F.png" style="width:166.5px; height:83px;"><strong style="font-size:27px;font-weight:bold;color:#3d3d3d;font-family:'Noto Sans KR',sans-serif;position:relative;bottom:3px;left:15px;">을 선택해야하는 이유</strong><br>
+                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px; font-family:'Noto Sans KR',sans-serif;">
+                다양성<span style="font-size:20px; color:#3d3d3d; margin-left:15px; font-weight:400;">Variety</span></p>
+                <span class="main5content">공장에서 찍어내는 듯한 웹/앱 서비스를 제공하지않습니다.</span>
+                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px; font-family:'Noto Sans KR',sans-serif;">
+                신뢰성<span style="font-size:20px; color:#3d3d3d; margin-left:15px; font-weight:400;">Reliability</span></p>
+                <span class="main5content">고객과 끊임없이 커뮤니케이션하며 고객이 가장 원하는 웹/앱서비스를<br>
+                제공합니다</span>
+                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px; font-family:'Noto Sans KR',sans-serif;">
+                효율성<span style="font-size:20px; color:#3d3d3d; margin-left:15px; font-weight:400;">Efficiency</span></p>
+                <span class="main5content">가장 합리적인 비용으로 최대의 서비스를 만들어냅니다.</span>
+                <p style="font-size:27px; color:#0066ff; font-weight:bold; margin-top:30px; font-family:'Noto Sans KR',sans-serif;">
+                보안성<span style="font-size:20px; color:#3d3d3d; margin-left:15px; font-weight:400;">Security</span> </p>
+                <span class="main5content">정보 보호를 위한 엄격한 보안을 준수합니다.</span>
             </div>
         </div>
     </div>   

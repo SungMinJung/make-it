5d3d8fd9ab1987c8f6395b62b6fea2460a8809ea
dongwook <donguk7531@hanmail.net>
2019-02-13 14:08:33 +0900

    test

--- resources/views/main/3.blade.php
+++ resources/views/main/3.blade.php
@@ -37,14 +37,16 @@
 
 </style>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <div class="row" style="background-image:url('/image/46.png'); width:1920px; height:330px; margin:0 auto;">
+        <div class="plain">
+        <div class="row" style="background-image:url('/image/46.png'); width:1920px; height:330px; margin:0 auto;">
         <div class="container"style="margin-top:30px;"> 
             <h1><p><strong>Mobile Appilcation</p></h1></strong>
             <h2><p style="margin-top:30px;">모바일 어플리케이션</p></h2><br>
             <button type="submit" class="btn gray" style="margin:0px auto; margin-top:30px;" onclick="location.href='{{ route('service.mobile') }}'">자세히 알아보기</button>
         </div>
     </div>
+</div>
+    
 
     <div class="row" style="background-image:url('/image/47.png'); width:1920px; height:330px; margin:0 auto;">
         <div class="container"style="margin-top:30px;"> 

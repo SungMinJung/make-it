8736c72cc81d7b7e1cc2e2e48c57df9fe2b9b0de
cbedp4109 <cbedp4109@gmail.com>
2019-02-18 10:35:52 +0900

    update추가

--- resources/views/admin/question/notice/edit.blade.php
+++ resources/views/admin/question/notice/edit.blade.php
@@ -10,31 +10,19 @@
 
             @component('gentelella.panel')
                 @slot('title')
-                    <h2>공지 추가</h2>
+                    <h2>공지 수정</h2>
                 @endslot
 
-
-                <form action="{{route('admin.notice.store')}}" class="form-horizontal form-label-left">
-
+                <form action="{{route('admin.notice.update',['id'=>$notices->id])}}"  class="form-horizontal form-label-left" method="post" >
+                    {{ csrf_field() }}
+                    {{ method_field('PUT')}}
+                        
                     <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="title">
-                            아이디 <span class="required">*</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            {{-- <input type="text" id="title" required="required" class="form-control col-md-7 col-xs-12"> --}}
-                            {{$notice->id}}
-
-                        </div>
-                    </div>
-    
-                    <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="title">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="subject">
                             제목 <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            {{-- <input type="text" id="title" required="required" class="form-control col-md-7 col-xs-12"> --}}
-                            {{$notice->subject}}
-
+                            <input type="text" value="{{$notices->subject}}" name="subject" id="subject" required="required" class="form-control col-md-7 col-xs-12">
                         </div>
                     </div>
 
@@ -43,16 +31,13 @@
                             내용 <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            {{-- <input type="content" id="content" required="required" class="form-control col-md-7 col-xs-12" style="height:250px;"> --}}
-                            
-                            {{-- <img src="{{$notice->img}}" alt="" style="width:100%;"> --}}
-                            {{$notice->content}}
+                        <input type="content" value="{{$notices->content}}" name="content" id="content" required="required" class="form-control col-md-7 col-xs-12" style="height:250px;">
                         </div>
                     </div>
-
-                    {{-- <div class="form-group">
+{{-- 
+                    <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">
-                             <span class="required">*</span>
+                            사진업로드 <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
                             <image-form src="/img/seongyeom.jpg"></image-form>
@@ -61,18 +46,16 @@
 
 
                     <div class="ln_solid"></div>
-                    <div class="form-group" style="text-align:center;">
+                    <div class="form-group">
                         <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                            <button type="submit"  class="btn btn-success">수정</button>
                             <a href="{{route('admin.notice.index')}}">
                                 <button class="btn btn-primary" type="button">취소</button>
                             </a>
                         </div>
                     </div>
 
-
                 </form>
-
-                
             @endcomponent
 
     </div>
